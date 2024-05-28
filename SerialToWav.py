a86cd4ecfeda2734907cf1bb2db461b663791a78
wararyo <wararyo@gmail.com>
2024-05-29 00:15:26 +0900

    調整

--- SerialToWav.py
+++ SerialToWav.py
@@ -6,19 +6,13 @@ import argparse
 import wave
 import struct
 
-# Serial MIDI Bridge
-# Ryan Kojima
-
-
-parser = argparse.ArgumentParser(description = "Serial MIDI bridge")
+parser = argparse.ArgumentParser(description = "Serial to wav converter")
 
 parser.add_argument("--serial_name", type=str, required = True, help = "Serial port name. Required")
 parser.add_argument("--baud", type=int, default=115200, help = "Baud rate. Default is 115200")
 parser.add_argument("--output", type=str, default="output.wav", help = "Output destination. Default is output.wav")
 
 args = parser.parse_args()
-
-thread_running = True
 
 # Arguments
 serial_port_name = args.serial_name
@@ -28,7 +22,7 @@ output_file_name = args.output
 # Initialize serial port
 try:
     ser = serial.Serial(serial_port_name,serial_baud)
-    ser.timeout = 0.4
+    ser.timeout = 1.0
 except serial.serialutil.SerialException:
     print("Serial port opening error.")
     sys.exit()
