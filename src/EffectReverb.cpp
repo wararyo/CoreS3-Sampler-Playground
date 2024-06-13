6ee747316931a7074449d5234e98bdfb2f3423be
wararyo <wararyo@gmail.com>
2024-06-14 00:27:32 +0900

    フィルターの遅延時間を4サンプル単位に変更
    金属的な響きが増えてしまったが、後でバンドパスフィルターを掛けるので許容

--- src/EffectReverb.cpp
+++ src/EffectReverb.cpp
@@ -27,7 +27,7 @@ void EffectReverb::Init()
                     REVERB_DELAY_BASIS_ALL_0 +
                     REVERB_DELAY_BASIS_ALL_1 +
                     REVERB_DELAY_BASIS_ALL_2 +
-                    7 * 8) &
+                    3 * 8) &
                    ~0b11) *
                   sizeof(float);
 #if defined(M5UNIFIED_PC_BUILD)
@@ -37,26 +37,33 @@ void EffectReverb::Init()
     memory = (float *)heap_caps_aligned_calloc(16, 1, size, MALLOC_CAP_INTERNAL);
 #endif
 
-    // 現状、timeは0.02〜1.0のみ対応
+    // 現状、timeは0.11〜1.0のみ対応
     if (time > 1.0) time = 1.0;
-    else if (time < 0.02) time = 0.02;
+    else if (time < 0.11) time = 0.11;
 
     // 各フィルター構造体の初期化 (バッファは16バイトアラインしておく)
     float *cursor = memory;
-    combs[0] = filter_t{cursor, cursor, 0.805f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_COMB_0)};
-    cursor += (REVERB_DELAY_BASIS_COMB_0 + 7) & ~0b11;
-    combs[1] = filter_t{cursor, cursor, 0.827f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_COMB_1)};
-    cursor += (REVERB_DELAY_BASIS_COMB_1 + 7) & ~0b11;
-    combs[2] = filter_t{cursor, cursor, 0.783f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_COMB_2)};
-    cursor += (REVERB_DELAY_BASIS_COMB_2 + 7) & ~0b11;
-    combs[3] = filter_t{cursor, cursor, 0.764f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_COMB_3)};
-    cursor += (REVERB_DELAY_BASIS_COMB_3 + 7) & ~0b11;
-    allpasses[0] = filter_t{cursor, cursor, 0.7f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_ALL_0)};
-    cursor += (REVERB_DELAY_BASIS_ALL_0 + 7) & ~0b11;
-    allpasses[1] = filter_t{cursor, cursor, 0.7f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_ALL_1)};
-    cursor += (REVERB_DELAY_BASIS_ALL_1 + 7) & ~0b11;
-    allpasses[2] = filter_t{cursor, cursor, 0.7f, cursor + (uint32_t)(time * REVERB_DELAY_BASIS_ALL_2)};
-    cursor += (REVERB_DELAY_BASIS_ALL_2 + 7) & ~0b11;
+    combs[0] = filter_t{cursor, cursor, 0.805f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_COMB_0) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_COMB_0 + 3) & ~0b11;
+    combs[1] = filter_t{cursor, cursor, 0.827f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_COMB_1) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_COMB_1 + 3) & ~0b11;
+    combs[2] = filter_t{cursor, cursor, 0.783f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_COMB_2) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_COMB_2 + 3) & ~0b11;
+    combs[3] = filter_t{cursor, cursor, 0.764f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_COMB_3) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_COMB_3 + 3) & ~0b11;
+    allpasses[0] = filter_t{cursor, cursor, 0.7f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_ALL_0) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_ALL_0 + 3) & ~0b11;
+    allpasses[1] = filter_t{cursor, cursor, 0.7f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_ALL_1) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_ALL_1 + 3) & ~0b11;
+    allpasses[2] = filter_t{cursor, cursor, 0.7f, cursor + ((uint32_t)(time * REVERB_DELAY_BASIS_ALL_2) & ~0b11)};
+    cursor += (REVERB_DELAY_BASIS_ALL_2 + 3) & ~0b11;
+
+    M5.Log.printf("memory: %x, comb0: %x-%x, comb1: %x-%x, comb3: %x-%x, comb4: %x-%x",
+        memory,
+        combs[0].buffer_start, combs[0].buffer_end,
+        combs[1].buffer_start, combs[1].buffer_end,
+        combs[2].buffer_start, combs[2].buffer_end,
+        combs[3].buffer_start, combs[3].buffer_end);
 }
 
 __attribute((optimize("-O3")))
