3ef7cb2e067229cda99070a115c56a5e470906c8
wararyo <wararyo@gmail.com>
2024-06-13 23:36:54 +0900

    EffectReverb ESP32S3時のアセンブリを仮で追加

--- src/EffectReverb.cpp
+++ src/EffectReverb.cpp
@@ -87,6 +87,57 @@ void comb_filter_process(const float *input, float *output, struct filter_t *com
         // コムフィルター処理
         // 一度に4サンプル処理するのでループ回数を1/4にする
         remain >>= 2;
+#if CONFIG_IDF_TARGET_ESP32S3
+        // ESP32S3の場合はSIMD命令を使って高速化
+        __asm__ (
+        // f0 | f1 -- f4 | f5 -- f8 | f9 -- f12 |
+        //  g | readback | outValue |  inValue  | 
+        "   wfr             f0, %4                  \n" // f0 = g
+        "   beqz.n          %0, .COMB_LOOP_END      \n"
+        "   loop            %0, .COMB_LOOP_END      \n" // remain回ループ
+        "   lsi             f1, %3, 0               \n" // readback0 = cursor[0]
+        "   lsi             f2, %3, 4               \n" // readback1 = cursor[1]
+        "   lsi             f3, %3, 8               \n" // readback2 = cursor[2]
+        "   lsi             f4, %3, 12              \n" // readback3 = cursor[3]
+        "   lsi             f5, %2, 0               \n" // outValue0 = output[0]
+        "   lsi             f6, %2, 4               \n" // outValue1 = output[1]
+        "   lsi             f7, %2, 8               \n" // outValue2 = output[2]
+        "   lsi             f8, %2, 12              \n" // outValue3 = output[3]
+        "   lsi             f9, %1, 0               \n" // inValue0 = input[0]
+        "   lsi             f10, %1, 4              \n" // inValue1 = input[1]
+        "   lsi             f11, %1, 8              \n" // inValue2 = input[2]
+        "   lsi             f12, %1, 12             \n" // inValue3 = input[3]
+        "   add.s           f5, f1, f5              \n" // outValue0 += readback0
+        "   add.s           f6, f2, f6              \n" // outValue1 += readback1
+        "   add.s           f7, f3, f7              \n" // outValue2 += readback2
+        "   add.s           f8, f4, f8              \n" // outValue3 += readback3
+        "   madd.s          f9, f0, f1              \n" // inValue0 += g * readback0
+        "   madd.s          f10, f0, f2             \n" // inValue1 += g * readback1
+        "   madd.s          f11, f0, f3             \n" // inValue2 += g * readback2
+        "   madd.s          f12, f0, f4             \n" // inValue3 += g * readback3
+        "   ssi             f5, %2, 0               \n" // output[0] = outValue0
+        "   ssi             f6, %2, 4               \n" // output[1] = outValue1
+        "   ssi             f7, %2, 8               \n" // output[2] = outValue2
+        "   ssi             f8, %2, 12              \n" // output[3] = outValue3
+        "   ssi             f9, %3, 0               \n" // cursor[0] = inValue0
+        "   ssi             f10, %3, 4              \n" // cursor[1] = inValue1
+        "   ssi             f11, %3, 8              \n" // cursor[2] = inValue2
+        "   ssi             f12, %3, 12             \n" // cursor[3] = inValue3
+        "   addi            %1, %1, 16              \n" // input += 4
+        "   addi            %3, %3, 16              \n" // cursor += 4
+        "   addi            %2, %2, 16              \n" // output += 4
+        ".COMB_LOOP_END:                            \n"
+        : // output-list            // アセンブリ言語からC/C++への受渡し
+        : // input-list             // C/C++からアセンブリ言語への受渡し
+            "r" ( remain ),         // %0 = remain
+            "r" ( input ),          // %1 = input
+            "r" ( output ),         // %2 = output
+            "r" ( cursor ),         // %3 = cursor
+            "r" ( g )               // %4 = g
+        : // clobber-list           // 値を書き換えたレジスタの申告
+            "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12"
+        );
+#else
         for (size_t i = 0; i < remain; i++)
         {
             const float readback0 = cursor[0];
@@ -117,7 +168,8 @@ void comb_filter_process(const float *input, float *output, struct filter_t *com
             cursor += 4;
             output += 4;
         }
-        
+#endif
+
         // バッファ終端と先頭をノイズなくつなげるための処理
         if (cursor >= buffer_end)
         {
@@ -167,6 +219,53 @@ void allpass_filter_process(const float *input, float *output, struct filter_t *
         // オールパスフィルター処理
         // 一度に4サンプル処理するのでループ回数を1/4にする
         remain >>= 2;
+#if CONFIG_IDF_TARGET_ESP32S3
+        // ESP32S3の場合はSIMD命令を使って高速化
+        __asm__ (
+        // f0 | f1 -- f4 | f5 -- f8 |
+        //  g | readback | newValue |
+        "   wfr             f0, %4                  \n" // f0 = g
+        "   beqz.n          %0, .ALLPASS_LOOP_END   \n"
+        "   loop            %0, .ALLPASS_LOOP_END   \n" // remain回ループ
+        "   lsi             f5, %1, 0               \n" // newValue0 = input[0]
+        "   lsi             f6, %1, 4               \n" // newValue1 = input[1]
+        "   lsi             f7, %1, 8               \n" // newValue2 = input[2]
+        "   lsi             f8, %1, 12              \n" // newValue3 = input[3]
+        "   lsi             f1, %3, 0               \n" // readback0 = cursor[0]
+        "   lsi             f2, %3, 4               \n" // readback1 = cursor[1]
+        "   lsi             f3, %3, 8               \n" // readback2 = cursor[2]
+        "   lsi             f4, %3, 12              \n" // readback3 = cursor[3]
+        "   msub.s          f1, f0, f5              \n" // readback0 -= g * newValue0
+        "   msub.s          f2, f0, f6              \n" // readback1 -= g * newValue1
+        "   msub.s          f3, f0, f7              \n" // readback2 -= g * newValue2
+        "   msub.s          f4, f0, f8              \n" // readback3 -= g * newValue3
+        "   madd.s          f5, f0, f1              \n" // newValue0 += g * readback0
+        "   madd.s          f6, f0, f2              \n" // newValue1 += g * readback1
+        "   madd.s          f7, f0, f3              \n" // newValue2 += g * readback2
+        "   madd.s          f8, f0, f4              \n" // newValue3 += g * readback3
+        "   ssi             f5, %3, 0               \n" // cursor[0] = newValue0
+        "   ssi             f6, %3, 4               \n" // cursor[1] = newValue1
+        "   ssi             f7, %3, 8               \n" // cursor[2] = newValue2
+        "   ssi             f8, %3, 12              \n" // cursor[3] = newValue3
+        "   ssi             f1, %2, 0               \n" // output[0] = readback0
+        "   ssi             f2, %2, 4               \n" // output[1] = readback1
+        "   ssi             f3, %2, 8               \n" // output[2] = readback2
+        "   ssi             f4, %2, 12              \n" // output[3] = readback3
+        "   addi            %1, %1, 16              \n" // input += 4
+        "   addi            %3, %3, 16              \n" // cursor += 4
+        "   addi            %2, %2, 16              \n" // output += 4
+        ".ALLPASS_LOOP_END:                         \n"
+        : // output-list            // アセンブリ言語からC/C++への受渡し
+        : // input-list             // C/C++からアセンブリ言語への受渡し
+            "r" ( remain ),         // %0 = remain
+            "r" ( input ),          // %1 = input
+            "r" ( output ),         // %2 = output
+            "r" ( cursor ),         // %3 = cursor
+            "r" ( g )               // %4 = g
+        : // clobber-list           // 値を書き換えたレジスタの申告
+            "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8"
+        );
+#else
         for (size_t i = 0; i < remain; i++)
         {
             float readback0 = cursor[0];
@@ -197,6 +296,7 @@ void allpass_filter_process(const float *input, float *output, struct filter_t *
             cursor += 4;
             output += 4;
         }
+#endif
 
         // バッファ終端と先頭をノイズなくつなげるための処理
         if (cursor >= buffer_end)
